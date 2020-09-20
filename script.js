3d70719a33ff8019bfed7181ccb4c455dc1f79e6
pjmantoss <joelptoss@gmail.com>
2020-09-20 13:51:27 +0100

    update script

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ const upBtn = document.querySelector("#up");
 const buyListDisplay = document.querySelector("#first-list");
 const fridgeListDisplay = document.querySelector("#second-list");
 
-const buyList = ['milk','pear','cucumber','protein shake','orange','spaghetti','egg','bread','peanut','soda'];
+const buyList = ['milk','pear','cucumber','protein shake','orange'];
 const fridge = [];
+
+//fill fridge with 5 items
+fridge.push('spaghetti','egg','bread','peanut','salad');
+
+//Some items on the buyList have been bought. Move them to the fridge
+let item = buyList.shift();
+fridge.unshift(item);
