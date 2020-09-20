8f69d99784e45e93338bce1d2a77036f5231dd91
pjmantoss <joelptoss@gmail.com>
2020-09-20 13:47:46 +0100

    update script

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ const upBtn = document.querySelector("#up");
 const buyListDisplay = document.querySelector("#first-list");
 const fridgeListDisplay = document.querySelector("#second-list");
 
+const buyList = ['milk','pear','cucumber','protein shake','orange','spaghetti','egg','bread','peanut','soda'];
+const fridge = [];
