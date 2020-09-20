c8b094d23304714831c911f665be77c04fe0ef22
pjmantoss <joelptoss@gmail.com>
2020-09-20 13:43:54 +0100

    update script

---
+++ script.js
@@ -0,0 +1,6 @@
+//Variables
+const downBtn = document.querySelector("#down");
+const upBtn = document.querySelector("#up");
+const buyListDisplay = document.querySelector("#first-list");
+const fridgeListDisplay = document.querySelector("#second-list");
+
